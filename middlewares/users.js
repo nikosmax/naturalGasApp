d07a18dc636a3b82e098da0c194dcb6e4f1896ec
unknown <stadio18@hotmail.com>
2017-06-12 00:13:30 +0300

    new file

---
+++ middlewares/users.js
@@ -0,0 +1,22 @@
+var express=require('express');
+var router=express.Router();
+var expressValidator = require('express-validator');
+
+router.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+module.exports = router;

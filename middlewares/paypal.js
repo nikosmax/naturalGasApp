7ba746a4974b49ff72f43d1078828670ff61be33
emddevelop <stadio18@hotmail.com>
2017-10-24 15:05:27 +0300

    files

--- middlewares/paypal.js
+++ middlewares/paypal.js
@@ -1,3 +1,9 @@
-/**
- * Created by Marina on 24/10/2017.
- */
+var paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'Ab8NnVTOnESuAf6IKF7x8roKNKw-SWaTK2wUAmJpbOYhXcwu2dXumgYjJxGIt0dMvVpo0CgOYLMG_o1i',
+    'client_secret': 'EImQg-JFfZ1ZVraLhxNEJJbK6SjLaGpIB_7LjusgFBAy1OvNW13Dc3QzqwAMo0ZGwzK8eqzAk1hiXQ5-'
+});
+
+module.exports=paypal;
